fef69b7a06ec17804a1604deb45212a6b53d57c8
dfriishecht <dfriishecht@olin.edu>
2026-02-01 19:43:33 -0500

    Implement get_robot_pose()

--- src/environment.py
+++ src/environment.py
@@ -93,8 +93,7 @@ class Environment:
         """
         Return the true robot pose.
         """
-        # TODO: fill in the function
-        pass
+        return self.robot_pose
 
     def get_proximity_to_landmarks(self) -> dict:
         """
