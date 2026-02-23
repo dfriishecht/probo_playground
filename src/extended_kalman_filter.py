dee305d583b9e65681035e2ad873bf1366fa7e41
dfriishecht <dfriishecht@olin.edu>
2026-02-23 18:47:23 -0500

    Update q

--- src/extended_kalman_filter.py
+++ src/extended_kalman_filter.py
@@ -135,7 +135,7 @@ class ExtendedKalmanFilter:
         """
         Generate white noise to apply to the process model after each prediction.
         """
-        stdev = 0.1
+        stdev = 0.01
         return np.array(
             [
                 [
